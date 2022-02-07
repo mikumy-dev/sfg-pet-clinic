f45bd1f68cc2e30638de1729d37c7f255d7f9b17
zosoyi <cjsz321qq@gmail.com>
2022-02-07 20:42:42 +0900

    Add Visit Services

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,48 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+    
+    private final VisitService visitService;
+
+    public VisitServiceMap(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit obj) {
+        super.delete(obj);
+    }
+
+    @Override
+    public Visit save(Visit obj) {
+        if (obj != null) {
+            if (obj.getPet() != null) {
+                return super.save(obj);
+            } else {
+                throw new RuntimeException("No Pet Visit");
+            }
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
